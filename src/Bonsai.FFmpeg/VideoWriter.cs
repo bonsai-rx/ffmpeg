08aff6bb6fe4bd7910545616c8cb799d5c0dc479
glopesdev <g.lopes@neurogears.org>
2025-11-11 23:15:37 +0000

    Add missing XML documentation strings

--- src/Bonsai.FFmpeg/VideoWriter.cs
+++ src/Bonsai.FFmpeg/VideoWriter.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
@@ -9,26 +9,59 @@ using System.IO;
 
 namespace Bonsai.FFmpeg
 {
+    /// <summary>
+    /// Represents an operator that writes a sequence of images to a video file using an FFmpeg process.
+    /// </summary>
     [DefaultProperty("FileName")]
-    [Description("Writes the input image sequence to an FFmpeg process.")]
+    [Description("Writes a sequence of images to a video file using an FFmpeg process.")]
     public class VideoWriter : Sink<IplImage>
     {
+        /// <summary>
+        /// Gets or sets the name of the output file.
+        /// </summary>
         [Description("The name of the output file.")]
         [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
         public string FileName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the optional suffix used to generate file names.
+        /// </summary>
         [Description("The optional suffix used to generate file names.")]
         public PathSuffix Suffix { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the output file should be overwritten if it already exists.
+        /// </summary>
         [Description("Indicates whether the output file should be overwritten if it already exists.")]
         public bool Overwrite { get; set; }
 
+        /// <summary>
+        /// Gets or sets the playback frame rate of the image sequence.
+        /// </summary>
         [Description("The playback frame rate of the image sequence.")]
         public int FrameRate { get; set; }
 
+        /// <summary>
+        /// Gets or sets the optional set of command-line arguments to use for configuring the video codec.
+        /// </summary>
         [Description("The optional set of command-line arguments to use for configuring the video codec.")]
         public string OutputArguments { get; set; }
 
+        /// <summary>
+        /// Writes an observable sequence of images to a video file using an FFmpeg process.
+        /// </summary>
+        /// <param name="source">
+        /// A sequence of <see cref="IplImage"/> objects representing the individual video
+        /// frames to include in the output file.
+        /// </param>
+        /// <returns>
+        /// An observable sequence that is identical to the <paramref name="source"/>
+        /// sequence but where there is an additional side effect of writing the
+        /// sequence of images to a video file using an FFmpeg process.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// The file already exists.
+        /// </exception>
         public override IObservable<IplImage> Process(IObservable<IplImage> source)
         {
             return source.Publish(ps =>
